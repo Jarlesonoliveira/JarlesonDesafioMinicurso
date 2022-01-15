374c93db55e2ac24b765eb23e1c64efa97ef809d
Jarleson oliveira <jarlesonmello@gmail.com>
2022-01-15 16:28:29 -0300

    Implementando função para gerar valor aléatorio e uma variavelpara numero aleatorio

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+gerarValorAleatorio = () => {
+    return math.floor(math.rondom() * 671);
+}
+
+pegarPersonagem = () => {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmorty.com/api/character/${numeroAleatorio}`)
+}
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
