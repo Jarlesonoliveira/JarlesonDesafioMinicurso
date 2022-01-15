8d74a3c75b22b15cffb1eac9ac6c37544bef151b
Jarleson oliveira <jarlesonmello@gmail.com>
2022-01-15 19:05:12 -0300

    Adicionar cor do bady

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-const imagem = document.querySelector('img');
+const imagemA = document.querySelector('img');
+const imagemB = document.querySelector('img');
+const imagemC = document.querySelector('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+const nomeDoPersonagemA = document.querySelector('#nome');
+const nomeDoPersonagemB = document.querySelector('#nome');
+const nomeDoPersonagemC = document.querySelector('#nome');
+const especieA = document.querySelector('#especie');
+const especieB = document.querySelector('#especie');
+const especieC = document.querySelector('#especie');
+const condicaoA = document.querySelector('#status');
+const condicaoB = document.querySelector('#status');
+const condicaoC = document.querySelector('#status');
 
 gerarValorAleatorio = () => {
-    return math.floor(math.rondom() * 671);
+    return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmorty.com/api/character/${numeroAleatorio}`, {
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
             "content-type": 'application/json'
         }
-    }).then((response) => response.json()).then((data) => {
-        imagem.src = data.image;
-        imagem.alt = data.name;
-        nomeDoPersonagem.innerHTML = data.species;
-        condicao.innerHTML = data.status;
 
-    }) 
+    }).then((response) => response.json()).then((data) => {
+        imagemA.src = data.image;
+        imagemA.alt = data.name;
+        nomeDoPersonagemA.innerHTML = data.name;
+        especieA.innerHTML = data.species;
+        condicaoA.innerHTML = data.status;
+    })
+    
+    .then((response) => response.json()).then((data) => {
+        imagemB.src = data.image;
+        imagemB.alt = data.name;
+        nomeDoPersonagemB.innerHTML = data.name;
+        especieB.innerHTML = data.especie;
+        condicaoB.innerHTML = data.status;
+
+    })
+
+    .then((response) => response.json()).then((data) => {
+        imagemC.src = data.image;
+        imagemC.alt = data.name;
+        nomeDoPersonagemC.innerHTML = data.name;
+        especieC.innerHTML = data.especie;
+        condicaoC.innerHTML = data.status;
+    }); 
 }
 
 botao.onclick = pegarPersonagem;
