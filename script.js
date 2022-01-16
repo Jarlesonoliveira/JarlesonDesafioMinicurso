a7a3fad267962e4d9f0531dbcd2360ac88340d24
Jarleson oliveira <jarlesonmello@gmail.com>
2022-01-16 12:49:46 -0300

    Implementando 2 imagens aleatorias na pagina

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const imagemA = document.querySelector('#imagem1');
 const nomeDoPersonagemA = document.querySelector('#nomeA');
 const especieA = document.querySelector('#especieA');
 const condicaoA = document.querySelector('#statusA');
+const imagemB = document.querySelector('#imagem2');
+const nomeDoPersonagemB = document.querySelector('#nomeB');
+const especieB = document.querySelector('#especieB');
+const condicaoB = document.querySelector('#statusB');
 const botao = document.querySelector('button');
 
 
@@ -24,6 +28,22 @@ pegarPersonagem = () => {
         nomeDoPersonagemA.innerHTML = data.name;
         especieA.innerHTML = data.species;
         condicaoA.innerHTML = data.status;
+    });
+
+    let numeroAleatorio2 = gerarValorAleatorio();
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio2}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "content-type": 'application/json'
+        }
+
+    }).then((response) => response.json()).then((data) => {
+        imagemB.src = data.image;
+        imagemB.alt = data.name;
+        nomeDoPersonagemB.innerHTML = data.name;
+        especieB.innerHTML = data.species;
+        condicaoB.innerHTML = data.status;
     });
 
 }
