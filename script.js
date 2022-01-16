eafb4f77b57f2c49edd181205a63dce23891ec96
Jarleson oliveira <jarlesonmello@gmail.com>
2022-01-16 12:36:20 -0300

    Implementando javascript com uma imagem aleatoria

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-const imagemA = document.querySelector('img');
-const imagemB = document.querySelector('img');
-const imagemC = document.querySelector('img');
+const imagemA = document.querySelector('#imagem1');
+const nomeDoPersonagemA = document.querySelector('#nomeA');
+const especieA = document.querySelector('#especieA');
+const condicaoA = document.querySelector('#statusA');
 const botao = document.querySelector('button');
-const nomeDoPersonagemA = document.querySelector('#nome');
-const nomeDoPersonagemB = document.querySelector('#nome');
-const nomeDoPersonagemC = document.querySelector('#nome');
-const especieA = document.querySelector('#especie');
-const especieB = document.querySelector('#especie');
-const especieC = document.querySelector('#especie');
-const condicaoA = document.querySelector('#status');
-const condicaoB = document.querySelector('#status');
-const condicaoC = document.querySelector('#status');
+
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    let numeroAleatorio1 = gerarValorAleatorio();
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio1}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
@@ -31,25 +24,8 @@ pegarPersonagem = () => {
         nomeDoPersonagemA.innerHTML = data.name;
         especieA.innerHTML = data.species;
         condicaoA.innerHTML = data.status;
-    })
-    
-    .then((response) => response.json()).then((data) => {
-        imagemB.src = data.image;
-        imagemB.alt = data.name;
-        nomeDoPersonagemB.innerHTML = data.name;
-        especieB.innerHTML = data.especie;
-        condicaoB.innerHTML = data.status;
+    });
 
-    })
-
-    .then((response) => response.json()).then((data) => {
-        imagemC.src = data.image;
-        imagemC.alt = data.name;
-        nomeDoPersonagemC.innerHTML = data.name;
-        especieC.innerHTML = data.especie;
-        condicaoC.innerHTML = data.status;
-    }); 
 }
 
 botao.onclick = pegarPersonagem;
-
